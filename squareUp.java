a14f1a5580e0a84bbf59a18d16163d9e194b3431
Israa Yousuf <141610564+wahaibisraa@users.noreply.github.com>
2025-08-03 11:04:35 +0400

    Delete squareUp.java

--- squareUp.java
+++
@@ -1,9 +0,0 @@
-public int[] squareUp(int n) {
-  int[] result = new int[n * n];
-  for (int i = 1; i <= n; i++) {
-    for (int j = 0; j < i; j++) {
-      result[i * n - 1 - j] = j + 1;
-    }
-  }
-  return result;
-}
